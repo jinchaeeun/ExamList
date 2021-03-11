33339e1058c1064e836299bd7f30e8f32105f82a
jinchaeeun <kbg05032@naver.com>
2021-03-11 12:02:09 +0900

    과제 DynamicList

--- app/src/main/java/com/example/examlist/DynamicList.java
+++ app/src/main/java/com/example/examlist/DynamicList.java
@@ -1,32 +1,43 @@
 package com.example.examlist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class DynamicList extends AppCompatActivity {
     private final Boolean           D = true;
     private final String            TAG = "MainActivity"; //프로젝트명이나 액티비티명
 
     //View object 관련
     private ListView                dataLST;
+    private EditText                nameETXT;
+    private Button                  addBTN;
 
     // List 관련
-    private String[]                arrDatas={"AA","BB","CC","DD"};
+    private ArrayList<String>       arrDatas;
     private ArrayAdapter<String>    adapter;
+
+    private InputMethodManager imm;
 
     //Member Method - Activity's Override -----------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dynamic);
         
         init();
 
@@ -36,15 +47,23 @@ public class MainActivity extends AppCompatActivity {
     //Member Method - Custom ---------------------------------
     // App 초기화 메서드
     private void init(){
+        //imm.hideSoftInputFromWindow(nameETXT.getWindowToken(), 0);
+
+        nameETXT = findViewById(R.id.nameETXT);
+        addBTN = findViewById(R.id.addBTN);
         dataLST = findViewById(R.id.dataLST);
         //ListView에 List 설정. 설정을 한다고하면 무조건 .set으로 시작!
 
         //List 데이터 준비
-        arrDatas = this.getResources().getStringArray(R.array.fruits);  //array는 getStringArray..
+        arrDatas = new ArrayList<String>();
+        //한번에 넣는 법
+        Collections.addAll(arrDatas, this.getResources().getStringArray(R.array.fruits));
+        //1개 넣는 법
+        //arrDatas.add("Test");
 
         //List 생성
         adapter = new ArrayAdapter<String>(this, 
-                                            android.R.layout.simple_list_item_1, 
+                                            android.R.layout.simple_list_item_1,
                                             arrDatas); //ArrayAdapter 더블 클릭 후 우클릭  Go To - implement해서 클릭하면 어떤 식으로 작성할 수 있는지 볼 수 있음.
                                                        // simple_list_items1은 우리가 id 만든게 아님
         // ListView에 List 설정
@@ -56,8 +75,23 @@ public class MainActivity extends AppCompatActivity {
                 TextView dataTXT = ((TextView)view.findViewById(android.R.id.text1)); //simple_list_itme_1의 textView id임
                 dataTXT.setText("클릭! 변경되었음");
                 //여기서는 Toast에 그냥 this하면 에러, MainActivity.this라고 써줘야함
-                Toast.makeText(MainActivity.this, "Text: "+(((TextView)view).getText())+", position: "+position + ", id: " + id, Toast.LENGTH_SHORT).show();
+                Toast.makeText(DynamicList.this, "Text: "+(((TextView)view).getText())+", position: "+position + ", id: " + id, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void click(View v){
+        //데이터 추가
+        if(nameETXT.getText().length()>0){
+            arrDatas.add(nameETXT.getText().toString());
+            initETXT();
+        }else{
+            Toast.makeText(DynamicList.this, R.string.check_msg, Toast.LENGTH_SHORT).show();;
+        }
+
+    }
+
+    private void initETXT(){
+        nameETXT.setText("");
     }
 }
